f1c2eda6edad931d05994d4f1b1c1171d292287a
robert morales <rmoralfit@gmail.com>
2021-05-04 17:22:33 -0400

    added POST & PATCH

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ document.addEventListener("DOMContentLoaded", fetchJokes);
 
 newJokeBtn.addEventListener('click', fetchJokes);
 
-constlaughBtn = document.querySelector(".laugh-btn");
+const likeBtn = document.querySelector(".like-btn");
 
+likeBtn.addEventListener("click",(e) => {
+    e.preventDefault()
+    postJoke()
+    })
 
 function fetchJokes() {
      fetch(url, {
@@ -23,5 +27,53 @@ function fetchJokes() {
     const h3 = document.createElement("h3")
     h3.innerText = joke
     document.querySelector(".container").append(h3)
+    const btn = document.createElement("button")
+    btn.innerText = `likes: ${joke.likes}`
+    //btn.id = joke.id
+
+    btn.addEventListener("click", likeJoke) 
+
  }
 
+ function postjoke() {
+    fetch(url, {
+      method: 'POST',
+      headers:
+       {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+  
+      body: JSON.stringify({
+        "likes": 0
+      })
+    })
+  
+      .then(response => response.json())
+      .then(data => renderJoke(data))
+      .catch(error => console.log(error))
+}
+  
+  function likeJoke(e) {
+
+    let more = parseInt(e.target.innerText) + 1 
+    const url2 = "https://icanhazdadjoke.com/" + e.target.id
+      fetch(url2, {
+      method: 'PATCH',
+      headers:
+       {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+  
+      body: JSON.stringify({
+        "likes": more
+      })
+    })
+  
+      .then(response => response.json())
+        .then(data => e.target.innerText = "likes: " + data.likes)
+        
+  }
+
+  
