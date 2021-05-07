020e9757f667b17e4f6231da7e203e34808dd9fc
robert morales <rmoralfit@gmail.com>
2021-05-06 21:19:01 -0400

    centered, adjusted font, enlarged counter to newJokeBtn

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ newJokeBtn.addEventListener('click', fetchJokes);
 newJokeBtn.addEventListener('click', () => {
     count = 0;
     counter.innerHTML = count; 
+    counter.className = "is-size-2 has-text-centered has-text-weight-bold"
 });
 
 likeBtn.addEventListener('click', play);
@@ -19,28 +20,29 @@ likeBtn.addEventListener('click', play);
 function play() {
     var audio = document.getElementById("audio");
     audio.play();
-  }
-
+};
 
 likeBtn.addEventListener('click', () => {
     count ++;
     counter.innerHTML = count; 
+    
 });
 
 function fetchJokes() {
      fetch(url, {
         headers: {
           Accept: 'application/json'
-        }
-      }) 
+    }
+}) 
         .then(response => response.json())
         .then(data => renderJoke(data.joke))
         .catch(error => console.error(error))
- }
+};
 
  function renderJoke(joke){
     const h3 = document.createElement("h3")
     h3.innerText = joke
+    h3.className = "is-size-4 has-text-black has-text-centered has-text-weight-bold"
     document.querySelector(".container").innerHTML = "";
     document.querySelector(".container").append(h3)
-}
+};
